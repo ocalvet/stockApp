04276a6c3384fb357f58092d3f4185efa1d56daf
Ovidio R. Calvet <ovidio@calvetsolutions.com>
2015-05-04 08:46:50 -0400

    Added stock search functionality

--- www/js/stockSearch/StockSearchCtrl.js
+++ www/js/stockSearch/StockSearchCtrl.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('stockSearch')
-    .controller('StockSearchCtrl', ['$scope', function($scope) {
+    .controller('StockSearchCtrl', ['$scope', 'urls', '$http', function($scope, urls, $http) {
+
+        $scope.comapnies = [];
+
+        $scope.searchCompany = function(term) {
+
+            var url = urls.search.replace('{{value}}', term);
+            $http
+                .get(url)
+                .success(function(companies) {
+                    console.log("Companies", companies);
+                    $scope.companies = companies;
+                })
+                .error(function(error) {
+                    console.log("There was an error retrieving company with term ", term);
+                });
+
+        }
 
     }]);
--- www/js/stockSearch/stockSearch.js
+++ www/js/stockSearch/stockSearch.js
@@ -1,3 +1,6 @@
 'use strict';
 
-angular.module('stockSearch', []);
+angular.module('stockSearch', [])
+    .constant('urls', {
+        search: 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={{value}}&callback=YAHOO.Finance.SymbolSuggest.ssCallback'
+    });
